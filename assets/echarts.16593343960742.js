894625501b81889c482062a5e1d7026dca17df23
zhaokunzhao <406123980@qq.com>
2022-08-01 14:19:31 +0800

    Revert "init"
    This reverts commit 9d672b6b0b972937e1cae95c83d66fc016b0bd47.

--- assets/echarts.16593343960742.js
+++
@@ -1,1 +0,0 @@
-import{v as a,w as s,x as l,y as t}from"./echarts.1659334396074.js";a([s,l]);a(t);
--- assets/logo-mini.1659334396074.js
+++
@@ -1,1 +0,0 @@
-var e="/vue-next-admin-preview/assets/logo-mini.1659334396074.svg";export{e as l};
